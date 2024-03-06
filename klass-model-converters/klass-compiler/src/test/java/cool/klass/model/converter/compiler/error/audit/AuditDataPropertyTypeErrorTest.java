3c78096bd93458a461994edd80eec1bd374b4085
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Better klass source code for compiler tests.

--- klass-model-converters/klass-compiler/src/test/java/cool/klass/model/converter/compiler/error/audit/AuditDataPropertyTypeErrorTest.java
+++ klass-model-converters/klass-compiler/src/test/java/cool/klass/model/converter/compiler/error/audit/AuditDataPropertyTypeErrorTest.java
@@ -1,8 +1,15 @@
 package cool.klass.model.converter.compiler.error.audit;
 
 import cool.klass.model.converter.compiler.error.AbstractKlassCompilerErrorTestCase;
+import org.junit.Test;
 
 public class AuditDataPropertyTypeErrorTest
         extends AbstractKlassCompilerErrorTestCase
 {
+    @Test
+    @Override
+    public void smokeTest()
+    {
+        this.assertNoCompilerErrors();
+    }
 }
--- klass-model-converters/klass-compiler/src/test/java/cool/klass/model/converter/compiler/error/user/UserIdTypeErrorTest.java
+++ klass-model-converters/klass-compiler/src/test/java/cool/klass/model/converter/compiler/error/user/UserIdTypeErrorTest.java
@@ -1,15 +1,8 @@
 package cool.klass.model.converter.compiler.error.user;
 
 import cool.klass.model.converter.compiler.error.AbstractKlassCompilerErrorTestCase;
-import org.junit.Test;
 
 public class UserIdTypeErrorTest
         extends AbstractKlassCompilerErrorTestCase
 {
-    @Test
-    @Override
-    public void smokeTest()
-    {
-        this.assertNoCompilerErrors();
-    }
 }
