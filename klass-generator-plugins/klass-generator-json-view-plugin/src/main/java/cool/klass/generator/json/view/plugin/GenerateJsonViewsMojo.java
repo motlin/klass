39f1080173f8595bf41d2bad02f476b685ca67d9
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Add module klass-generator-json-view-plugin.

---
+++ klass-generator-plugins/klass-generator-json-view-plugin/src/main/java/cool/klass/generator/json/view/plugin/GenerateJsonViewsMojo.java
@@ -0,0 +1,52 @@
+package cool.klass.generator.json.view.plugin;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Instant;
+
+import cool.klass.generator.json.view.JsonViewGenerator;
+import cool.klass.generator.plugin.AbstractGenerateMojo;
+import cool.klass.model.meta.domain.api.DomainModel;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+
+@Mojo(
+        name = "generate-json-views",
+        defaultPhase = LifecyclePhase.GENERATE_SOURCES,
+        threadSafe = true,
+        requiresDependencyResolution = ResolutionScope.RUNTIME)
+public class GenerateJsonViewsMojo extends AbstractGenerateMojo
+{
+    @Parameter(property = "outputDirectory", defaultValue = "${project.build.directory}/generated-sources/json-views")
+    private File outputDirectory;
+
+    @Parameter(property = "applicationName", required = true, readonly = true)
+    private String applicationName;
+
+    @Parameter(property = "rootPackageName", required = true, readonly = true)
+    private String rootPackageName;
+
+    @Override
+    public void execute() throws MojoExecutionException
+    {
+        DomainModel domainModel = this.getDomainModel();
+        try
+        {
+            JsonViewGenerator jsonViewGenerator = new JsonViewGenerator(
+                    domainModel,
+                    this.rootPackageName,
+                    this.applicationName,
+                    Instant.now());
+            jsonViewGenerator.writeJsonViews(this.outputDirectory.toPath());
+        }
+        catch (IOException e)
+        {
+            throw new MojoExecutionException(e.getMessage(), e);
+        }
+
+        this.mavenProject.addCompileSourceRoot(this.outputDirectory.getPath());
+    }
+}
