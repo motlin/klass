3c76ad2942dba2857058fb870766964f48232ddc
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Add a test to klass-compiler for forward references.

---
+++ klass-model-converters/klass-compiler/src/test/java/cool/klass/model/converter/compiler/annotation/general/ForwardReferenceErrorTest.java
@@ -0,0 +1,8 @@
+package cool.klass.model.converter.compiler.annotation.general;
+
+import cool.klass.model.converter.compiler.annotation.AbstractKlassCompilerErrorTestCase;
+
+public class ForwardReferenceErrorTest
+        extends AbstractKlassCompilerErrorTestCase
+{
+}
