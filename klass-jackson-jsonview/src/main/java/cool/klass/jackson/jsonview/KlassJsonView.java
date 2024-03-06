94f363e3d141f69b52c0884ca034d125e23c8602
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Add module klass-jackson-jsonview.

---
+++ klass-jackson-jsonview/src/main/java/cool/klass/jackson/jsonview/KlassJsonView.java
@@ -0,0 +1,9 @@
+package cool.klass.jackson.jsonview;
+
+import cool.klass.model.meta.domain.api.projection.Projection;
+
+// TODO: Javadoc this
+public interface KlassJsonView
+{
+    Projection getProjection();
+}
