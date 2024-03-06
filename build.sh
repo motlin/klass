71f010905c929ea68e74b6e53e4f5f4be0a52580
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Set up initial project with

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -euxo pipefail
 rm -rf .m2/cool/klass/
 rm -rf ~/.m2/repository/cool/klass/
 
-mvn -N io.takari:maven:wrapper -Dmaven=3.6.2
+mvn -N io.takari:maven:wrapper -Dmaven=3.6.3
 ./mvnw de.qaware.maven:go-offline-maven-plugin:1.1.0:resolve-dependencies --activate-profiles !shade,!run-demo,enforcer-strict
 # ./mvnw --threads 1C --offline --activate-profiles !shade,!run-demo,enforcer-strict
 ./mvnw --offline --activate-profiles !shade,!run-demo,enforcer-strict -DforkCount=0
