7d3c79f7631c0d7ee039c29952d6a6300849357f
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Sync up most recent changes from archetype to StackOverflow.

--- xample-projects/stackoverflow/stackoverflow-reladomo-pojos/src/main/java/com/stackoverflow/User.java
+++ xample-projects/stackoverflow/stackoverflow-reladomo-pojos/src/main/java/com/stackoverflow/User.java
@@ -6,15 +6,15 @@ import com.gs.fw.common.mithra.util.DefaultInfinityTimestamp;
 
 public class User extends UserAbstract
 {
-    public User()
-    {
-        this(DefaultInfinityTimestamp.getDefaultInfinity());
-    }
-
     public User(Timestamp system)
     {
         super(system);
         // You must not modify this constructor. Mithra calls this internally.
         // You can call this constructor. You can also add new constructors.
+    }
+
+    public User()
+    {
+        this(DefaultInfinityTimestamp.getDefaultInfinity());
     }
 }
