52be8d07bf62cb4430946befacb92d9955e11ee8
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Revert "Refactor modifiers."
    This reverts commit c9d76fec

---
+++ klass-model-converters/klass-compiler/src/main/java/cool/klass/model/converter/compiler/state/modifier/AntlrModifierOwner.java
@@ -0,0 +1,7 @@
+package cool.klass.model.converter.compiler.state.modifier;
+
+import cool.klass.model.converter.compiler.state.IAntlrElement;
+
+public interface AntlrModifierOwner extends IAntlrElement
+{
+}
