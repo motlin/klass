0c81e974ae99a8d2229a86cba26224be95604a39
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Change ReladomoResponseBundle to dynamically check the type of its config.

--- klass-reladomo/klass-dropwizard-bundle-reladomo-response/src/main/java/cool/klass/dropwizard/bundle/reladomo/response/ReladomoResponseBundle.java
+++ klass-reladomo/klass-dropwizard-bundle-reladomo-response/src/main/java/cool/klass/dropwizard/bundle/reladomo/response/ReladomoResponseBundle.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.google.auto.service.AutoService;
 import cool.klass.data.store.DataStore;
+import cool.klass.dropwizard.configuration.data.store.DataStoreFactory;
 import cool.klass.dropwizard.configuration.data.store.DataStoreFactoryProvider;
 import cool.klass.serialization.jackson.response.KlassResponse;
 import cool.klass.serialization.jackson.response.reladomo.KlassResponseReladomoJsonSerializer;
@@ -16,7 +17,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @AutoService(PrioritizedBundle.class)
-public class ReladomoResponseBundle implements PrioritizedBundle<DataStoreFactoryProvider>
+public class ReladomoResponseBundle
+        implements PrioritizedBundle<Object>
 {
     private static final Logger LOGGER = LoggerFactory.getLogger(ReladomoResponseBundle.class);
 
@@ -32,11 +34,16 @@ public class ReladomoResponseBundle implements PrioritizedBundle<DataStoreFactor
     }
 
     @Override
-    public void run(@Nonnull DataStoreFactoryProvider configuration, @Nonnull Environment environment)
+    public void run(@Nonnull Object configuration, @Nonnull Environment environment)
     {
+        DataStoreFactoryProvider dataStoreFactoryProvider = this.safeCastConfiguration(
+                DataStoreFactoryProvider.class,
+                configuration);
+
         LOGGER.info("Running {}.", ReladomoResponseBundle.class.getSimpleName());
 
-        DataStore dataStore = configuration.getDataStoreFactory().createDataStore();
+        DataStoreFactory dataStoreFactory = dataStoreFactoryProvider.getDataStoreFactory();
+        DataStore        dataStore        = dataStoreFactory.createDataStore();
 
         JsonSerializer<KlassResponse> serializer = new KlassResponseReladomoJsonSerializer(dataStore);
 
