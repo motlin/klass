c722039a60162359a56b76e229b8c0b1b5e31c61
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Change both clock and domain model to be loaded using dropwizard config.

--- klass-dropwizard/klass-dropwizard-application/src/main/java/cool/klass/dropwizard/application/AbstractKlassApplication.java
+++ klass-dropwizard/klass-dropwizard-application/src/main/java/cool/klass/dropwizard/application/AbstractKlassApplication.java
@@ -1,6 +1,5 @@
 package cool.klass.dropwizard.application;
 
-import java.time.Clock;
 import java.util.Objects;
 import java.util.ServiceLoader;
 
@@ -9,10 +8,6 @@ import javax.annotation.Nonnull;
 import ch.qos.logback.classic.Level;
 import cool.klass.dropwizard.bundle.prioritized.PrioritizedBundle;
 import cool.klass.dropwizard.configuration.AbstractKlassConfiguration;
-import cool.klass.dropwizard.configuration.KlassFactory;
-import cool.klass.dropwizard.configuration.clock.ClockFactory;
-import cool.klass.dropwizard.configuration.domain.model.loader.DomainModelFactory;
-import cool.klass.model.meta.domain.api.DomainModel;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import com.typesafe.config.ConfigRenderOptions;
@@ -29,9 +24,6 @@ public abstract class AbstractKlassApplication<T extends AbstractKlassConfigurat
     private static final Logger LOGGER = LoggerFactory.getLogger(AbstractKlassApplication.class);
 
     protected final String name;
-
-    protected DomainModel domainModel;
-    protected Clock       clock;
 
     protected AbstractKlassApplication(String name)
     {
@@ -108,23 +100,7 @@ public abstract class AbstractKlassApplication<T extends AbstractKlassConfigurat
     }
 
     @Override
-    public void run(@Nonnull T configuration, Environment environment) throws Exception
+    public void run(@Nonnull T configuration, @Nonnull Environment environment) throws Exception
     {
-        this.initializeClock(configuration);
-        this.initializeDomainModel(configuration);
-    }
-
-    protected void initializeClock(@Nonnull T configuration)
-    {
-        KlassFactory klassFactory = configuration.getKlassFactory();
-        ClockFactory clockFactory = klassFactory.getClockFactory();
-        this.clock = clockFactory.getClock();
-    }
-
-    protected void initializeDomainModel(@Nonnull T configuration)
-    {
-        KlassFactory       klassFactory       = configuration.getKlassFactory();
-        DomainModelFactory domainModelFactory = klassFactory.getDomainModelFactory();
-        this.domainModel = domainModelFactory.getDomainModel();
     }
 }
