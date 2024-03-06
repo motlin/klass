bf6b4aa23ad5bbc768895fe5946ee89a3e52a7cb
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Add module klass-generator-projection-plugin.

---
+++ klass-generator-plugins/klass-generator-projection-plugin/src/main/java/cool/klass/generator/klass/projection/plugin/GenerateProjectionMojo.java
@@ -0,0 +1,34 @@
+package cool.klass.generator.klass.projection.plugin;
+
+import java.io.File;
+
+import cool.klass.generator.klass.projection.KlassProjectionGenerator;
+import cool.klass.generator.plugin.AbstractGenerateMojo;
+import cool.klass.model.meta.domain.api.DomainModel;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+
+@Mojo(
+        name = "generate-klass-projection",
+        defaultPhase = LifecyclePhase.GENERATE_SOURCES,
+        threadSafe = true,
+        requiresDependencyResolution = ResolutionScope.RUNTIME)
+public class GenerateProjectionMojo
+        extends AbstractGenerateMojo
+{
+    @Parameter(
+            property = "outputDirectory",
+            defaultValue = "${project.build.directory}/generated-sources/projection")
+    private File outputDirectory;
+
+    @Override
+    public void execute() throws MojoExecutionException
+    {
+        DomainModel domainModel = this.getDomainModel();
+        KlassProjectionGenerator generator = new KlassProjectionGenerator(domainModel);
+        generator.writeKlassFiles(this.outputDirectory.toPath());
+    }
+}
