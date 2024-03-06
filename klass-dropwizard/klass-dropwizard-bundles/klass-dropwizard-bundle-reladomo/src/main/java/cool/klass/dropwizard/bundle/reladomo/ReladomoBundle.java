7ff2c63436358f4f7f785e136c54c4b52d96c925
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Fold klass-reladomo-configuration into klass-dropwizard-bundle-reladomo.

--- klass-dropwizard/klass-dropwizard-bundles/klass-dropwizard-bundle-reladomo/src/main/java/cool/klass/dropwizard/bundle/reladomo/ReladomoBundle.java
+++ klass-dropwizard/klass-dropwizard-bundles/klass-dropwizard-bundle-reladomo/src/main/java/cool/klass/dropwizard/bundle/reladomo/ReladomoBundle.java
@@ -1,15 +1,17 @@
 package cool.klass.dropwizard.bundle.reladomo;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import javax.annotation.Nonnull;
 
 import com.google.auto.service.AutoService;
+import com.gs.fw.common.mithra.MithraManager;
 import com.gs.fw.common.mithra.MithraManagerProvider;
 import cool.klass.dropwizard.bundle.prioritized.PrioritizedBundle;
 import cool.klass.dropwizard.configuration.reladomo.ReladomoFactory;
 import cool.klass.dropwizard.configuration.reladomo.ReladomoFactoryProvider;
-import cool.klass.reladomo.configuration.ReladomoConfig;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import io.dropwizard.util.Duration;
@@ -39,17 +41,19 @@ public class ReladomoBundle implements PrioritizedBundle<ReladomoFactoryProvider
 
         ReladomoFactory reladomoFactory = configuration.getReladomoFactory();
 
-        Duration     transactionTimeout                     = reladomoFactory.getTransactionTimeout();
-        int          transactionTimeoutSeconds              = Math.toIntExact(transactionTimeout.toSeconds());
-        List<String> runtimeConfigurationPaths              = reladomoFactory.getRuntimeConfigurationPaths();
-        boolean      enableRetrieveCountMetrics             = reladomoFactory.isEnableRetrieveCountMetrics();
-        boolean      captureTransactionLevelPerformanceData = reladomoFactory.isCaptureTransactionLevelPerformanceData();
+        Duration     transactionTimeout         = reladomoFactory.getTransactionTimeout();
+        int          transactionTimeoutSeconds  = Math.toIntExact(transactionTimeout.toSeconds());
+        List<String> runtimeConfigurationPaths  = reladomoFactory.getRuntimeConfigurationPaths();
+        boolean      enableRetrieveCountMetrics = reladomoFactory.isEnableRetrieveCountMetrics();
 
-        ReladomoConfig.setTransactionTimeout(transactionTimeoutSeconds);
+        boolean captureTransactionLevelPerformanceData =
+                reladomoFactory.isCaptureTransactionLevelPerformanceData();
+
+        setTransactionTimeout(transactionTimeoutSeconds);
         // Notification should be configured here. Refer to notification/Notification.html under reladomo-javadoc.jar.
-        ReladomoConfig.loadRuntimeConfigurations(runtimeConfigurationPaths);
+        loadRuntimeConfigurations(runtimeConfigurationPaths);
 
-        ReladomoConfig.setCaptureTransactionLevelPerformanceData(captureTransactionLevelPerformanceData);
+        setCaptureTransactionLevelPerformanceData(captureTransactionLevelPerformanceData);
 
         if (enableRetrieveCountMetrics)
         {
@@ -67,5 +71,37 @@ public class ReladomoBundle implements PrioritizedBundle<ReladomoFactoryProvider
         environment.metrics().gauge(
                 "Reladomo remote retrieve count",
                 () -> () -> MithraManagerProvider.getMithraManager().getRemoteRetrieveCount());
+    }
+
+    private static void setTransactionTimeout(int transactionTimeoutSeconds)
+    {
+        MithraManager mithraManager = MithraManagerProvider.getMithraManager();
+        mithraManager.setTransactionTimeout(transactionTimeoutSeconds);
+    }
+
+    private static void setCaptureTransactionLevelPerformanceData(boolean captureTransactionLevelPerformanceData)
+    {
+        MithraManager mithraManager = MithraManagerProvider.getMithraManager();
+        mithraManager.setCaptureTransactionLevelPerformanceData(captureTransactionLevelPerformanceData);
+    }
+
+    private static void loadRuntimeConfigurations(@Nonnull List<String> runtimeConfigurationPaths)
+    {
+        runtimeConfigurationPaths.forEach(ReladomoBundle::loadRuntimeConfiguration);
+    }
+
+    private static void loadRuntimeConfiguration(String runtimeConfigurationPath)
+    {
+        LOGGER.info("Loading Reladomo configuration XML: {}", runtimeConfigurationPath);
+        try (
+                InputStream inputStream = ReladomoBundle.class.getClassLoader()
+                        .getResourceAsStream(runtimeConfigurationPath))
+        {
+            MithraManagerProvider.getMithraManager().readConfiguration(inputStream);
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
     }
 }
