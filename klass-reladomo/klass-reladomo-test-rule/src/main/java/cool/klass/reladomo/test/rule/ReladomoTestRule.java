6b9099871a23b795943e82cb5988f16a8a833073
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Add module klass-reladomo-test-rule.

---
+++ klass-reladomo/klass-reladomo-test-rule/src/main/java/cool/klass/reladomo/test/rule/ReladomoTestRule.java
@@ -0,0 +1,68 @@
+package cool.klass.reladomo.test.rule;
+
+import com.gs.fw.common.mithra.test.ConnectionManagerForTests;
+import com.gs.fw.common.mithra.test.MithraTestResource;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+public class ReladomoTestRule implements TestRule
+{
+    private final String   runtimeConfigFilename;
+    private final String[] testDataFileNames;
+
+    private MithraTestResource mithraTestResource;
+
+    public ReladomoTestRule(
+            String runtimeConfigFilename,
+            String... testDataFileNames)
+    {
+        this.runtimeConfigFilename = runtimeConfigFilename;
+        this.testDataFileNames = testDataFileNames;
+    }
+
+    @Override
+    public Statement apply(Statement base, Description description)
+    {
+        ReladomoTestFile reladomoTestFileAnnotation = description.getAnnotation(ReladomoTestFile.class);
+        String[] testDataFileNames = reladomoTestFileAnnotation == null
+                ? this.testDataFileNames
+                : reladomoTestFileAnnotation.value();
+
+        return new Statement()
+        {
+            @Override
+            public void evaluate() throws Throwable
+            {
+                ReladomoTestRule.this.before(testDataFileNames);
+                try
+                {
+                    base.evaluate();
+                }
+                finally
+                {
+                    ReladomoTestRule.this.after();
+                }
+            }
+        };
+    }
+
+    private void before(String[] testDataFileNames)
+    {
+        this.mithraTestResource = new MithraTestResource(this.runtimeConfigFilename);
+        // TODO: Make the test database name configurable
+        ConnectionManagerForTests connectionManager = ConnectionManagerForTests.getInstance("testdb");
+        this.mithraTestResource.createSingleDatabase(connectionManager);
+
+        for (String testDataFileName : testDataFileNames)
+        {
+            this.mithraTestResource.addTestDataToDatabase(testDataFileName, connectionManager);
+        }
+        this.mithraTestResource.setUp();
+    }
+
+    private void after()
+    {
+        this.mithraTestResource.tearDown();
+    }
+}
