1644d69f137e63748e6217fcb397457a52ee8036
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Add module klass-generator-graphql-schema-query.

---
+++ klass-generator-plugins/klass-generator-graphql-schema-query-plugin/src/main/java/cool/klass/generator/graphql/schema/query/plugin/GenerateGraphQLSchemaQueryMojo.java
@@ -0,0 +1,58 @@
+package cool.klass.generator.graphql.schema.query.plugin;
+
+import java.io.File;
+import java.nio.file.Path;
+
+import cool.klass.generator.grahql.schema.query.GraphQLSchemaQueryGenerator;
+import cool.klass.generator.plugin.AbstractGenerateMojo;
+import cool.klass.model.meta.domain.api.DomainModel;
+import org.apache.maven.model.Resource;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+
+@Mojo(
+        name = "generate-graphql-schema-query",
+        defaultPhase = LifecyclePhase.GENERATE_RESOURCES,
+        threadSafe = true,
+        requiresDependencyResolution = ResolutionScope.RUNTIME)
+public class GenerateGraphQLSchemaQueryMojo
+        extends AbstractGenerateMojo
+{
+    @Parameter(
+            property = "outputDirectory",
+            defaultValue = "${project.build.directory}/generated-resources/graphql-schema-query")
+    private File outputDirectory;
+
+    @Parameter(property = "applicationName", required = true, readonly = true)
+    private String applicationName;
+
+    @Parameter(property = "rootPackageName", required = true, readonly = true)
+    private String rootPackageName;
+
+    @Override
+    public void execute() throws MojoExecutionException
+    {
+        if (!this.outputDirectory.exists())
+        {
+            this.outputDirectory.mkdirs();
+        }
+
+        DomainModel domainModel = this.getDomainModel();
+
+        Path outputPath = this.outputDirectory.toPath();
+
+        GraphQLSchemaQueryGenerator generator = new GraphQLSchemaQueryGenerator(
+                domainModel,
+                this.rootPackageName,
+                this.applicationName);
+        generator.writeQueryFile(outputPath);
+
+        Resource resource = new Resource();
+        resource.setDirectory(this.outputDirectory.getAbsolutePath());
+        // TODO: Should be based on the output path
+        this.mavenProject.addResource(resource);
+    }
+}
