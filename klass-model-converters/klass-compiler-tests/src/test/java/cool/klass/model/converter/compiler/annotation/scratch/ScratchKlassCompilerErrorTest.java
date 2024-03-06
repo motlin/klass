62d7c5353453df3c4f09f5e6651ae93881393dbd
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Scratch klass file for compiler error experiments.

---
+++ klass-model-converters/klass-compiler-tests/src/test/java/cool/klass/model/converter/compiler/annotation/scratch/ScratchKlassCompilerErrorTest.java
@@ -0,0 +1,16 @@
+package cool.klass.model.converter.compiler.annotation.scratch;
+
+import cool.klass.model.converter.compiler.annotation.AbstractKlassCompilerErrorTestCase;
+import org.junit.Test;
+
+public class ScratchKlassCompilerErrorTest
+        extends AbstractKlassCompilerErrorTestCase
+{
+    @Test
+    @Override
+    public void smokeTest()
+    {
+        // TODO:
+        this.assertNoCompilerErrors();
+    }
+}
