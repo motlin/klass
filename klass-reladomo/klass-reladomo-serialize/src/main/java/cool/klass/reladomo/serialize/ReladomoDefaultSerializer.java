11e3054bcba65b0d3104c6be5c139188727176a4
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Add module klass-reladomo-serialize.

---
+++ klass-reladomo/klass-reladomo-serialize/src/main/java/cool/klass/reladomo/serialize/ReladomoDefaultSerializer.java
@@ -0,0 +1,43 @@
+package cool.klass.reladomo.serialize;
+
+import javax.annotation.Nonnull;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gs.fw.common.mithra.MithraObject;
+import com.gs.fw.common.mithra.finder.RelatedFinder;
+import com.gs.fw.common.mithra.util.serializer.SerializationConfig;
+import com.gs.fw.common.mithra.util.serializer.Serialized;
+import cool.klass.jackson.ObjectMapperConfig;
+import io.dropwizard.jackson.Jackson;
+
+public final class ReladomoDefaultSerializer
+{
+    private ReladomoDefaultSerializer()
+    {
+        throw new AssertionError("Suppress default constructor for noninstantiability");
+    }
+
+    public static String serialize(@Nonnull MithraObject mithraObject)
+    {
+        ObjectMapper objectMapper = Jackson.newObjectMapper();
+        ObjectMapperConfig.configure(objectMapper);
+        return serialize(mithraObject, objectMapper);
+    }
+
+    public static String serialize(@Nonnull MithraObject mithraObject, @Nonnull ObjectMapper objectMapper)
+    {
+        RelatedFinder            finder              = mithraObject.zGetPortal().getFinder();
+        SerializationConfig      serializationConfig = SerializationConfig.shallowWithDefaultAttributes(finder);
+        Serialized<MithraObject> serialized          = new Serialized<>(mithraObject, serializationConfig);
+
+        try
+        {
+            return objectMapper.writeValueAsString(serialized);
+        }
+        catch (JsonProcessingException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+}
