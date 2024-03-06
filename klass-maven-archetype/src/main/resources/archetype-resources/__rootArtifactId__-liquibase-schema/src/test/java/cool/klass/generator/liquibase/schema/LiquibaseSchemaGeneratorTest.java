be2468528e3f58ae494ade83bc8fa4161aac75ff
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Add __rootArtifactId__-liquibase-schema to klass-maven-archetype.

--- klass-maven-archetype/src/main/resources/archetype-resources/__rootArtifactId__-liquibase-schema/src/test/java/cool/klass/generator/liquibase/schema/LiquibaseSchemaGeneratorTest.java
+++ klass-maven-archetype/src/main/resources/archetype-resources/__rootArtifactId__-liquibase-schema/src/test/java/cool/klass/generator/liquibase/schema/LiquibaseSchemaGeneratorTest.java
@@ -1,18 +1,19 @@
-package cool.klass.generator.klass.service;
+package cool.klass.generator.liquibase.schema;
 
-import cool.klass.model.meta.domain.api.PackageableElement;
 import cool.klass.model.meta.domain.api.source.DomainModelWithSourceCode;
 import cool.klass.model.meta.loader.compiler.DomainModelCompilerLoader;
 import io.liftwizard.junit.rule.log.marker.LogMarkerTestRule;
 import io.liftwizard.junit.rule.match.file.FileMatchRule;
-import org.eclipse.collections.api.factory.Lists;
 import org.eclipse.collections.api.list.ImmutableList;
+import org.eclipse.collections.impl.factory.Lists;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestRule;
 
-public class KlassServiceGeneratorTest
+public class LiquibaseSchemaGeneratorTest
 {
+    public static final String FULLY_QUALIFIED_PACKAGE = "${package}";
+
     @Rule
     public final FileMatchRule fileMatchRule = new FileMatchRule(this.getClass());
 
@@ -22,7 +23,7 @@ public class KlassServiceGeneratorTest
     @Test
     public void smokeTest()
     {
-        ImmutableList<String> klassSourcePackages = Lists.immutable.with("${package}");
+        ImmutableList<String> klassSourcePackages = Lists.immutable.with(FULLY_QUALIFIED_PACKAGE);
 
         var domainModelCompilerLoader = new DomainModelCompilerLoader(
                 klassSourcePackages,
@@ -30,20 +31,9 @@ public class KlassServiceGeneratorTest
                 DomainModelCompilerLoader::logCompilerError);
 
         DomainModelWithSourceCode domainModel = domainModelCompilerLoader.load();
-        ImmutableList<String> packageNames = domainModel
-                .getClassifiers()
-                .asLazy()
-                .collect(PackageableElement::getPackageName)
-                .distinct()
-                .toImmutableList();
-        for (String packageName : packageNames)
-        {
-            String sourceCode                = KlassServiceSourceCodeGenerator.getPackageSourceCode(domainModel, packageName);
-            String resourceClassPathLocation = packageName + ".klass";
 
-            this.fileMatchRule.assertFileContents(
-                    resourceClassPathLocation,
-                    sourceCode);
-        }
+        this.fileMatchRule.assertFileContents(
+                this.getClass().getCanonicalName() + ".xml",
+                SchemaGenerator.getSourceCode(domainModel, FULLY_QUALIFIED_PACKAGE));
     }
 }
