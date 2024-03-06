fe0e7456534954ea5e89dac6c2829a6a3daf78c3
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Move strictest enforcer rule into a maven profile, to enable running --resume-from builds.

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -euxo pipefail
 rm -rf .m2/cool/klass/
 rm -rf ~/.m2/repository/cool/klass/
 
-mvn -N io.takari:maven:wrapper -Dmaven=3.6.1
-./mvnw de.qaware.maven:go-offline-maven-plugin:1.1.0:resolve-dependencies --activate-profiles !shade,!run-demo --no-transfer-progress
-# ./mvnw -T1C clean install --activate-profiles !shade,!run-demo
-./mvnw --offline --no-transfer-progress --activate-profiles !shade,!run-demo -DforkCount=0
+mvn -N io.takari:maven:wrapper -Dmaven=3.6.2
+./mvnw de.qaware.maven:go-offline-maven-plugin:1.1.0:resolve-dependencies --activate-profiles !shade,!run-demo,enforcer-strict
+# ./mvnw --threads 1C --offline --activate-profiles !shade,!run-demo,enforcer-strict
+./mvnw --offline --activate-profiles !shade,!run-demo,enforcer-strict -DforkCount=0
