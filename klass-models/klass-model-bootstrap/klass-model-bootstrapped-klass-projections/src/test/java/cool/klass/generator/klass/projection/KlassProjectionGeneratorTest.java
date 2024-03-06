2f5c6167ec636f3eac6c4c5ec37d943657956cc0
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Add module klass-model-bootstrapped-klass-projections. Make assertions about generated Klass source.

---
+++ klass-models/klass-model-bootstrap/klass-model-bootstrapped-klass-projections/src/test/java/cool/klass/generator/klass/projection/KlassProjectionGeneratorTest.java
@@ -0,0 +1,49 @@
+package cool.klass.generator.klass.projection;
+
+import cool.klass.model.meta.domain.api.PackageableElement;
+import cool.klass.model.meta.domain.api.source.DomainModelWithSourceCode;
+import cool.klass.model.meta.loader.compiler.DomainModelCompilerLoader;
+import io.liftwizard.junit.rule.log.marker.LogMarkerTestRule;
+import io.liftwizard.junit.rule.match.file.FileMatchRule;
+import org.eclipse.collections.api.factory.Lists;
+import org.eclipse.collections.api.list.ImmutableList;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestRule;
+
+public class KlassProjectionGeneratorTest
+{
+    @Rule
+    public final FileMatchRule fileMatchRule = new FileMatchRule(this.getClass());
+
+    @Rule
+    public final TestRule logMarkerTestRule = new LogMarkerTestRule();
+
+    @Test
+    public void smokeTest()
+    {
+        ImmutableList<String> klassSourcePackages = Lists.immutable.with("klass.model.meta.domain");
+
+        var domainModelCompilerLoader = new DomainModelCompilerLoader(
+                klassSourcePackages,
+                Thread.currentThread().getContextClassLoader(),
+                DomainModelCompilerLoader::logCompilerError);
+
+        DomainModelWithSourceCode domainModel = domainModelCompilerLoader.load();
+        ImmutableList<String>     packageNames = domainModel
+                .getClassifiers()
+                .asLazy()
+                .collect(PackageableElement::getPackageName)
+                .distinct()
+                .toImmutableList();
+        for (String packageName : packageNames)
+        {
+            String sourceCode                = KlassProjectionGenerator.getPackageSourceCode(domainModel, packageName);
+            String resourceClassPathLocation = packageName + ".klass";
+
+            this.fileMatchRule.assertFileContents(
+                    resourceClassPathLocation,
+                    sourceCode);
+        }
+    }
+}
