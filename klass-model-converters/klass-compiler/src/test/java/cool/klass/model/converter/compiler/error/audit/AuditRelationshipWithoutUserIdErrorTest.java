860873c130158d9c6c73e43d61a22498d8974822
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Better compiler error tests.

--- klass-model-converters/klass-compiler/src/test/java/cool/klass/model/converter/compiler/error/audit/AuditRelationshipWithoutUserIdErrorTest.java
+++ klass-model-converters/klass-compiler/src/test/java/cool/klass/model/converter/compiler/error/audit/AuditRelationshipWithoutUserIdErrorTest.java
@@ -1,8 +1,15 @@
 package cool.klass.model.converter.compiler.error.audit;
 
 import cool.klass.model.converter.compiler.error.AbstractKlassCompilerErrorTestCase;
+import org.junit.Test;
 
 public class AuditRelationshipWithoutUserIdErrorTest
         extends AbstractKlassCompilerErrorTestCase
 {
+    @Test
+    @Override
+    public void smokeTest()
+    {
+        this.assertNoCompilerErrors();
+    }
 }
