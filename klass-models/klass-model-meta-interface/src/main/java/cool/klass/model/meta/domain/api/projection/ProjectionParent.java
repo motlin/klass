579a6aa66d83cbe2976456b825c7333642095431
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Add helper methods to ProjectionParent.

--- klass-models/klass-model-meta-interface/src/main/java/cool/klass/model/meta/domain/api/projection/ProjectionParent.java
+++ klass-models/klass-model-meta-interface/src/main/java/cool/klass/model/meta/domain/api/projection/ProjectionParent.java
@@ -3,7 +3,9 @@ package cool.klass.model.meta.domain.api.projection;
 import javax.annotation.Nonnull;
 
 import cool.klass.model.meta.domain.api.Klass;
+import cool.klass.model.meta.domain.api.property.AssociationEnd;
 import org.eclipse.collections.api.list.ImmutableList;
+import org.eclipse.collections.impl.factory.Lists;
 
 public interface ProjectionParent extends ProjectionElement
 {
@@ -12,4 +14,29 @@ public interface ProjectionParent extends ProjectionElement
 
     @Override
     ImmutableList<ProjectionElement> getChildren();
+
+    default ImmutableList<ProjectionAssociationEnd> getAssociationEndChildren()
+    {
+        return this.getChildren().selectInstancesOf(ProjectionAssociationEnd.class);
+    }
+
+    default ImmutableList<AssociationEnd> getAssociationEnds()
+    {
+        return this.getAssociationEndChildren().collect(ProjectionAssociationEnd::getAssociationEnd);
+    }
+
+    default ImmutableList<AssociationEnd> getAssociationEndsOutsideProjection()
+    {
+        ImmutableList<AssociationEnd> associationEndsInProjection = this.getAssociationEnds();
+
+        ImmutableList<AssociationEnd> optionalReturnPath = Lists.immutable.with(this)
+                .selectInstancesOf(ProjectionAssociationEnd.class)
+                .collect(ProjectionAssociationEnd::getAssociationEnd)
+                .collect(AssociationEnd::getOpposite);
+
+        return this.getKlass()
+                .getAssociationEnds()
+                .reject(associationEndsInProjection::contains)
+                .reject(optionalReturnPath::contains);
+    }
 }
