9f1eb8e186ed5bd0bd3e0f4e2cec41d4e75c6864
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Use dynamic bundles without dynamically loading.

--- klass-models/klass-model-bootstrap/klass-model-bootstrapped-dropwizard-application/src/main/java/klass/model/meta/domain/dropwizard/application/KlassBootstrappedMetaModelApplication.java
+++ klass-models/klass-model-bootstrap/klass-model-bootstrapped-dropwizard-application/src/main/java/klass/model/meta/domain/dropwizard/application/KlassBootstrappedMetaModelApplication.java
@@ -1,9 +1,41 @@
 package klass.model.meta.domain.dropwizard.application;
 
+import javax.annotation.Nonnull;
+
+import cool.klass.dropwizard.bundle.reladomo.jsonview.ReladomoJsonViewBundle;
+import cool.klass.dropwizard.bundle.reladomo.response.ReladomoResponseBundle;
+import com.liftwizard.dropwizard.bundle.ddl.executor.DdlExecutorBundle;
+import com.liftwizard.dropwizard.bundle.h2.H2Bundle;
+import com.liftwizard.dropwizard.bundle.named.data.source.NamedDataSourceBundle;
+import com.liftwizard.dropwizard.bundle.reladomo.ReladomoBundle;
+import com.liftwizard.dropwizard.bundle.reladomo.connection.manager.ConnectionManagerBundle;
+import com.liftwizard.dropwizard.bundle.reladomo.connection.manager.holder.ConnectionManagerHolderBundle;
+import io.dropwizard.setup.Bootstrap;
+
 public class KlassBootstrappedMetaModelApplication extends AbstractKlassBootstrappedMetaModelApplication
 {
     public static void main(String[] args) throws Exception
     {
         new KlassBootstrappedMetaModelApplication().run(args);
+    }
+
+    @Override
+    protected void initializeDynamicBundles(@Nonnull Bootstrap<KlassBootstrappedMetaModelConfiguration> bootstrap)
+    {
+    }
+
+    @Override
+    protected void initializeBundles(@Nonnull Bootstrap<KlassBootstrappedMetaModelConfiguration> bootstrap)
+    {
+        super.initializeBundles(bootstrap);
+
+        bootstrap.addBundle(new H2Bundle());
+        bootstrap.addBundle(new NamedDataSourceBundle());
+        bootstrap.addBundle(new DdlExecutorBundle());
+        bootstrap.addBundle(new ConnectionManagerBundle());
+        bootstrap.addBundle(new ConnectionManagerHolderBundle());
+        bootstrap.addBundle(new ReladomoBundle());
+        bootstrap.addBundle(new ReladomoJsonViewBundle());
+        bootstrap.addBundle(new ReladomoResponseBundle());
     }
 }
