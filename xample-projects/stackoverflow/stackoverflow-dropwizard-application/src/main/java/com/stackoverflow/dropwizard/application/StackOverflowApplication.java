8bae0b4badfb4243d98fe7476c8ac8b541f45e73
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Move manual configuration of bundles and commands up into generated abstract application.

--- xample-projects/stackoverflow/stackoverflow-dropwizard-application/src/main/java/com/stackoverflow/dropwizard/application/StackOverflowApplication.java
+++ xample-projects/stackoverflow/stackoverflow-dropwizard-application/src/main/java/com/stackoverflow/dropwizard/application/StackOverflowApplication.java
@@ -1,23 +1,10 @@
 package com.stackoverflow.dropwizard.application;
 
-import cool.klass.dropwizard.bundle.httplogging.HttpLoggingBundle;
-import cool.klass.dropwizard.bundle.objectmapper.ObjectMapperBundle;
-import cool.klass.dropwizard.bundle.reladomo.ReladomoBundle;
-import cool.klass.model.meta.domain.DomainModel;
-import cool.klass.model.meta.loader.DomainModelLoader;
-import com.stackoverflow.dropwizard.command.StackOverflowTestDataGeneratorCommand;
-import com.typesafe.config.Config;
-import com.typesafe.config.ConfigFactory;
-import com.typesafe.config.ConfigRenderOptions;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class StackOverflowApplication extends AbstractStackOverflowApplication
 {
-    private static final Logger LOGGER = LoggerFactory.getLogger(StackOverflowApplication.class);
-
     public static void main(String[] args) throws Exception
     {
         new StackOverflowApplication().run(args);
@@ -28,11 +15,7 @@ public class StackOverflowApplication extends AbstractStackOverflowApplication
     {
         super.initialize(bootstrap);
 
-        bootstrap.addCommand(new StackOverflowTestDataGeneratorCommand(this));
-
-        bootstrap.addBundle(new HttpLoggingBundle());
-        bootstrap.addBundle(new ObjectMapperBundle());
-        bootstrap.addBundle(new ReladomoBundle());
+        // TODO: application initialization
     }
 
     @Override
@@ -40,23 +23,6 @@ public class StackOverflowApplication extends AbstractStackOverflowApplication
             StackOverflowConfiguration configuration,
             Environment environment)
     {
-        Config config      = ConfigFactory.load();
-        Config klassConfig = config.getConfig("klass");
-
-        if (LOGGER.isInfoEnabled())
-        {
-            ConfigRenderOptions configRenderOptions = ConfigRenderOptions.defaults()
-                    .setJson(false)
-                    .setOriginComments(false);
-            String render = klassConfig.root().render(configRenderOptions);
-            LOGGER.info("Klass configuration:\n{}", render);
-        }
-
-        String rootPackage = klassConfig.getString("rootPackage");
-
-        DomainModelLoader domainModelLoader = new DomainModelLoader(rootPackage);
-        DomainModel       domainModel       = domainModelLoader.load();
-
-        this.run(configuration, environment, domainModel);
+        super.run(configuration, environment);
     }
 }
