3112283638c99b411da0decdf16056d9ef38af85
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Change the configuration of the maven-shade-plugin so that it's disabled through a profile called 'dev'.

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -euxo pipefail
 # Remove artifacts we are about to build from local caches
 rm -rf .m2/cool/klass/
 
-./mvnw de.qaware.maven:go-offline-maven-plugin:1.2.8:resolve-dependencies --activate-profiles !shade -Dmaven.repo.local='.m2'
-#./mvnw --threads 2C --offline --activate-profiles !shade -Dmaven.repo.local='.m2' $@
-./mvnw --offline --activate-profiles !shade -DforkCount=0 -Dmaven.repo.local='.m2' $@
+./mvnw de.qaware.maven:go-offline-maven-plugin:1.2.8:resolve-dependencies --activate-profiles dev -Dmaven.repo.local='.m2'
+#./mvnw --threads 2C --offline --activate-profiles dev -Dmaven.repo.local='.m2' $@
+./mvnw --offline --activate-profiles dev -DforkCount=0 -Dmaven.repo.local='.m2' $@
