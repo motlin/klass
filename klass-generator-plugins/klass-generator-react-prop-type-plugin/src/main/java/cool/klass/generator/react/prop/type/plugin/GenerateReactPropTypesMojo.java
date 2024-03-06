d7764551bfdf1c593d7f3b69f2c4ac1971fcd17c
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Add module klass-generator-react-prop-type-plugin.

---
+++ klass-generator-plugins/klass-generator-react-prop-type-plugin/src/main/java/cool/klass/generator/react/prop/type/plugin/GenerateReactPropTypesMojo.java
@@ -0,0 +1,43 @@
+package cool.klass.generator.react.prop.type.plugin;
+
+import java.io.File;
+import java.time.Instant;
+
+import cool.klass.generator.plugin.AbstractGenerateMojo;
+import cool.klass.generator.react.prop.type.ReactPropTypeGenerator;
+import cool.klass.model.meta.domain.api.DomainModel;
+import org.apache.maven.model.Resource;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+
+@Mojo(
+        name = "generate-react-prop-types",
+        defaultPhase = LifecyclePhase.GENERATE_SOURCES,
+        threadSafe = true,
+        requiresDependencyResolution = ResolutionScope.RUNTIME)
+public class GenerateReactPropTypesMojo extends AbstractGenerateMojo
+{
+    @Parameter(
+            property = "outputDirectory",
+            defaultValue = "${project.build.directory}/generated-resources/react-prop-types")
+    private File outputDirectory;
+
+    @Override
+    public void execute() throws MojoExecutionException
+    {
+        DomainModel domainModel = this.getDomainModel();
+        ReactPropTypeGenerator propTypeGenerator = new ReactPropTypeGenerator(
+                domainModel,
+                Instant.now());
+        propTypeGenerator.writePropTypes(this.outputDirectory.toPath());
+
+        Resource resource = new Resource();
+        resource.setDirectory(this.outputDirectory.getAbsolutePath());
+        // TODO: Should be based on the output path
+        resource.setTargetPath("react-prop-types");
+        this.mavenProject.addResource(resource);
+    }
+}
