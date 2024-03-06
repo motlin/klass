cfeeb8953e067dcc6dc0bda6164449e69826d0c9
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Change QuestionResourceManualTest to extend AbstractDropwizardAppTest.

--- xample-projects/stackoverflow/stackoverflow-dropwizard-application/src/test/java/com/stackoverflow/dropwizard/test/QuestionResourceManualTest.java
+++ xample-projects/stackoverflow/stackoverflow-dropwizard-application/src/test/java/com/stackoverflow/dropwizard/test/QuestionResourceManualTest.java
@@ -7,78 +7,26 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
 import com.stackoverflow.dropwizard.application.StackOverflowApplication;
-import com.stackoverflow.dropwizard.application.StackOverflowConfiguration;
-import io.dropwizard.client.JerseyClientBuilder;
-import io.dropwizard.client.JerseyClientConfiguration;
 import io.dropwizard.testing.ResourceHelpers;
 import io.dropwizard.testing.junit.DropwizardAppRule;
-import io.dropwizard.util.Duration;
-import io.liftwizard.junit.rule.log.marker.LogMarkerTestRule;
+import io.liftwizard.dropwizard.testing.junit.AbstractDropwizardAppTest;
 import io.liftwizard.junit.rule.match.file.FileMatchRule;
-import io.liftwizard.junit.rule.match.json.JsonMatchRule;
-import io.liftwizard.reladomo.test.rule.ReladomoLoadDataTestRule;
 import io.liftwizard.reladomo.test.rule.ReladomoTestFile;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.RuleChain;
-import org.junit.rules.TestRule;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 
 public class QuestionResourceManualTest
+        extends AbstractDropwizardAppTest
 {
-    @Rule
-    public final JsonMatchRule jsonMatchRule = new JsonMatchRule(this.getClass());
-
-    protected final DropwizardAppRule<StackOverflowConfiguration> appRule = new DropwizardAppRule<>(
-            StackOverflowApplication.class,
-            ResourceHelpers.resourceFilePath("config-test.json5"));
-
-    protected final TestRule reladomoLoadDataTestRule = new ReladomoLoadDataTestRule();
-    protected final TestRule logMarkerTestRule        = new LogMarkerTestRule();
-
-    @Rule
-    public final TestRule rule = RuleChain
-            .outerRule(this.appRule)
-            .around(this.reladomoLoadDataTestRule)
-            .around(this.logMarkerTestRule);
-
-    protected Client getClient(String testName)
+    @Nonnull
+    @Override
+    protected DropwizardAppRule getDropwizardAppRule()
     {
-        var jerseyClientConfiguration = new JerseyClientConfiguration();
-        jerseyClientConfiguration.setTimeout(Duration.minutes(5));
-
-        String className  = this.getClass().getCanonicalName();
-        String clientName = className + "." + testName;
-
-        return new JerseyClientBuilder(this.appRule.getEnvironment())
-                .using(jerseyClientConfiguration)
-                .build(clientName);
-    }
-
-    protected void assertEmptyResponse(Status expectedStatus, Response actualResponse)
-    {
-        assertFalse(actualResponse.hasEntity());
-        assertThat(actualResponse.getStatusInfo(), is(expectedStatus));
-    }
-
-    protected void assertResponse(String testName, Status expectedStatus, Response actualResponse)
-    {
-        this.assertResponseStatus(actualResponse, expectedStatus);
-        String actualJsonResponse = actualResponse.readEntity(String.class);
-
-        String expectedResponseClassPathLocation = this.getClass().getSimpleName() + "." + testName + ".json";
-
-        this.jsonMatchRule.assertFileContents(expectedResponseClassPathLocation, actualJsonResponse);
-    }
-
-    protected void assertResponseStatus(@Nonnull Response response, Status status)
-    {
-        response.bufferEntity();
-        String entityAsString = response.readEntity(String.class);
-        assertThat(entityAsString, response.getStatusInfo(), is(status));
+        return new DropwizardAppRule<>(
+                StackOverflowApplication.class,
+                ResourceHelpers.resourceFilePath("config-test.json5"));
     }
 
     @Test
