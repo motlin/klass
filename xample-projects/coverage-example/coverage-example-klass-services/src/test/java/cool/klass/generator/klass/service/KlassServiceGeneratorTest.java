d947202a9c4301a2469ff8ccccb566fc77b18020
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Add back coverage-example-klass-services as a module, and change it to make file match assertions about the generated services.

---
+++ xample-projects/coverage-example/coverage-example-klass-services/src/test/java/cool/klass/generator/klass/service/KlassServiceGeneratorTest.java
@@ -0,0 +1,47 @@
+package cool.klass.generator.klass.service;
+
+import cool.klass.model.meta.domain.api.PackageableElement;
+import cool.klass.model.meta.domain.api.source.DomainModelWithSourceCode;
+import cool.klass.model.meta.loader.compiler.DomainModelCompilerLoader;
+import io.liftwizard.junit.rule.log.marker.LogMarkerTestRule;
+import io.liftwizard.junit.rule.match.file.FileMatchRule;
+import org.eclipse.collections.api.factory.Lists;
+import org.eclipse.collections.api.list.ImmutableList;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestRule;
+
+public class KlassServiceGeneratorTest
+{
+    @Rule
+    public final FileMatchRule fileMatchRule = new FileMatchRule();
+
+    @Rule
+    public final TestRule logMarkerTestRule = new LogMarkerTestRule();
+
+    @Test
+    public void smokeTest()
+    {
+        ImmutableList<String> klassSourcePackages = Lists.immutable.with("cool.klass.xample.coverage");
+
+        var domainModelCompilerLoader = new DomainModelCompilerLoader(klassSourcePackages);
+
+        DomainModelWithSourceCode domainModel = domainModelCompilerLoader.load();
+        ImmutableList<String> packageNames = domainModel
+                .getClassifiers()
+                .asLazy()
+                .collect(PackageableElement::getPackageName)
+                .distinct()
+                .toImmutableList();
+        for (String packageName : packageNames)
+        {
+            String sourceCode                = KlassServiceGenerator.getPackageSourceCode(domainModel, packageName);
+            String resourceClassPathLocation = packageName + ".klass";
+
+            this.fileMatchRule.assertFileContents(
+                    resourceClassPathLocation,
+                    sourceCode,
+                    this.getClass());
+        }
+    }
+}
