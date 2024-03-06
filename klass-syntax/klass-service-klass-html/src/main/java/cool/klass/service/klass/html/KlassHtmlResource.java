0ce19c05cf566d247e9aa2fb8f889a33d1ec5ea0
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Fix KlassHtmlResource for top level elements and enumeration literals.

--- klass-syntax/klass-service-klass-html/src/main/java/cool/klass/service/klass/html/KlassHtmlResource.java
+++ klass-syntax/klass-service-klass-html/src/main/java/cool/klass/service/klass/html/KlassHtmlResource.java
@@ -1,8 +1,5 @@
 package cool.klass.service.klass.html;
 
-import java.net.URI;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -14,16 +11,15 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import com.codahale.metrics.annotation.ExceptionMetered;
 import com.codahale.metrics.annotation.Timed;
 import cool.klass.generator.klass.html.KlassSourceCodeHtmlGenerator;
-import cool.klass.model.meta.domain.api.TopLevelElement;
+import cool.klass.model.meta.domain.api.EnumerationLiteral;
 import cool.klass.model.meta.domain.api.property.Property;
-import cool.klass.model.meta.domain.api.service.ServiceGroup;
 import cool.klass.model.meta.domain.api.source.DomainModelWithSourceCode;
 import cool.klass.model.meta.domain.api.source.ElementWithSourceCode;
+import cool.klass.model.meta.domain.api.source.EnumerationWithSourceCode;
 import cool.klass.model.meta.domain.api.source.KlassWithSourceCode;
 import cool.klass.model.meta.domain.api.source.SourceCode;
 import cool.klass.model.meta.domain.api.source.TopLevelElementWithSourceCode;
@@ -45,28 +41,21 @@ public class KlassHtmlResource
     @GET
     @Path("/meta/code/element/{topLevelElementName}")
     @Produces(MediaType.TEXT_HTML)
-    public Response topLevelElementNameSourceCode(@PathParam("topLevelElementName") String topLevelElementName)
+    public String topLevelElementNameSourceCode(@PathParam("topLevelElementName") String topLevelElementName)
     {
-        // TODO: Implement DomainModel.getTopLevelElementByName(name)
-        ImmutableList<TopLevelElement> topLevelElements = this.domainModel
-                .getTopLevelElements()
-                .reject(ServiceGroup.class::isInstance)
-                .select(each -> each.getName().equals(topLevelElementName));
+        TopLevelElementWithSourceCode topLevelElement = this.domainModel
+                .getTopLevelElementByName(topLevelElementName);
 
-        if (topLevelElements.size() != 1)
+        Optional<SourceCode> sourceCode = getSourceCodeObject(topLevelElement, null);
+        if (sourceCode.isEmpty())
         {
             throw new BadRequestException();
         }
-        TopLevelElementWithSourceCode topLevelElement   = (TopLevelElementWithSourceCode) topLevelElements.getOnly();
-        SourceCode                    sourceCode        = topLevelElement.getSourceCodeObject();
-        String                        sourceName        = sourceCode.getSourceName();
-        String                        encodedSourceName = URLEncoder.encode(sourceName, StandardCharsets.UTF_8);
-        String uriString = String.format(
-                "/api/meta/code/file/%s.html#%s",
-                encodedSourceName,
-                topLevelElementName);
-        URI location = URI.create(uriString);
-        return Response.temporaryRedirect(location).build();
+        return KlassSourceCodeHtmlGenerator.getSourceCode(
+                this.domainModel,
+                sourceCode.get(),
+                Optional.of(topLevelElement),
+                Optional.empty());
     }
 
     @Timed
@@ -109,6 +98,21 @@ public class KlassHtmlResource
                 return Optional.empty();
             }
             if (property.get() instanceof ElementWithSourceCode elementWithSourceCode)
+            {
+                return Optional.of(elementWithSourceCode.getSourceCodeObject());
+            }
+        }
+
+        if (topLevelElement instanceof EnumerationWithSourceCode enumeration)
+        {
+            Optional<EnumerationLiteral> enumerationLiteral = enumeration
+                    .getEnumerationLiterals()
+                    .detectOptional(each -> each.getName().equals(memberName));
+            if (enumerationLiteral.isEmpty())
+            {
+                return Optional.empty();
+            }
+            if (enumerationLiteral.get() instanceof ElementWithSourceCode elementWithSourceCode)
             {
                 return Optional.of(elementWithSourceCode.getSourceCodeObject());
             }
