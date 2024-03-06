d91b259d491010007323f86a28cb037699fe22bd
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Isolate maven builds by using local .m2 repository.

--- build.sh
+++ build.sh
@@ -3,11 +3,8 @@
 set -euxo pipefail
 
 # Remove artifacts we are about to build from local caches
-rm -rf .m2/io/liftwizard/
-rm -rf ~/.m2/repository/io/liftwizard/
 rm -rf .m2/cool/klass/
-rm -rf ~/.m2/repository/cool/klass/
 
-./mvnw de.qaware.maven:go-offline-maven-plugin:1.1.0:resolve-dependencies --activate-profiles !shade,!run-demo,enforcer-strict
-./mvnw --threads 2C --offline --activate-profiles !shade,!run-demo,enforcer-strict $@
-#./mvnw --offline --activate-profiles !shade,!run-demo,enforcer-strict -DforkCount=0 $@
+./mvnw de.qaware.maven:go-offline-maven-plugin:1.1.0:resolve-dependencies --activate-profiles !shade,!run-demo,enforcer-strict -Dmaven.repo.local='.m2'
+#./mvnw --threads 2C --offline --activate-profiles !shade,!run-demo,enforcer-strict -Dmaven.repo.local='.m2' $@
+./mvnw --offline --activate-profiles !shade,!run-demo,enforcer-strict -DforkCount=0 -Dmaven.repo.local='.m2' $@
