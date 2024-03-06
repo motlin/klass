237eec71a849904e20dbd8e9b5448ed16f24c868
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Set up initial project with: - build.sh - CircleCI build - .gitignore - .idea - .mvn

--- build.sh
+++ build.sh
@@ -6,7 +6,6 @@ set -euxo pipefail
 rm -rf .m2/cool/klass/
 rm -rf ~/.m2/repository/cool/klass/
 
-mvn -N io.takari:maven:wrapper -Dmaven=3.6.3
 ./mvnw de.qaware.maven:go-offline-maven-plugin:1.1.0:resolve-dependencies --activate-profiles !shade,!run-demo,enforcer-strict
-# ./mvnw --threads 1C --offline --activate-profiles !shade,!run-demo,enforcer-strict
-./mvnw --offline --activate-profiles !shade,!run-demo,enforcer-strict -DforkCount=0
+./mvnw --threads 2C --offline --activate-profiles !shade,!run-demo,enforcer-strict $@
+#./mvnw --offline --activate-profiles !shade,!run-demo,enforcer-strict -DforkCount=0 $@
