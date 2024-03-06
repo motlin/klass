85e22bbb82826fa7474ba2d15f730ed5a8f988fa
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Fix klass-compiler-plugin to bind to a later maven phase, after resources are put on the classpath.

--- klass-generator-plugins/klass-compiler-plugin/src/main/java/cool/klass/compiler/plugin/KlassCompilerMojo.java
+++ klass-generator-plugins/klass-compiler-plugin/src/main/java/cool/klass/compiler/plugin/KlassCompilerMojo.java
@@ -8,7 +8,7 @@ import org.apache.maven.plugins.annotations.Mojo;
 /**
  * This plugin doesn't emit anything. It just compiles the source code and emits compiler errors if applicable, in order to get compiler errors in the correct module. This helps with rerunnability.
  */
-@Mojo(name = "compile", defaultPhase = LifecyclePhase.GENERATE_SOURCES, threadSafe = true)
+@Mojo(name = "compile", defaultPhase = LifecyclePhase.TEST, threadSafe = true)
 public class KlassCompilerMojo extends AbstractGenerateMojo
 {
     @Override
