aeeb3c8a5894d1321df569aa54d22ec8515c0a6d
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Add DerivedPropertyOrderByErrorTest. Ordering by a derived property should either result in a compiler error, or ought to be supported in-memory.

---
+++ klass-model-converters/klass-compiler-tests/src/test/java/cool/klass/model/converter/compiler/annotation/property/DerivedPropertyOrderByErrorTest.java
@@ -0,0 +1,17 @@
+package cool.klass.model.converter.compiler.annotation.property;
+
+import cool.klass.model.converter.compiler.annotation.AbstractKlassCompilerErrorTestCase;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class DerivedPropertyOrderByErrorTest
+        extends AbstractKlassCompilerErrorTestCase
+{
+    @Test
+    @Ignore("Doesn't fail yet")
+    @Override
+    public void smokeTest()
+    {
+        super.smokeTest();
+    }
+}
