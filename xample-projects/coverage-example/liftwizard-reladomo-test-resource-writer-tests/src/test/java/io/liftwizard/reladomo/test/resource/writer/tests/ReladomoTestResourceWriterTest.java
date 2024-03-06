f6e10babd556669243404135457d808eb82c28f3
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Fix null-handling in liftwizard-reladomo-test-resource-writer. Move temporal columns to the beginning.

--- xample-projects/coverage-example/liftwizard-reladomo-test-resource-writer-tests/src/test/java/io/liftwizard/reladomo/test/resource/writer/tests/ReladomoTestResourceWriterTest.java
+++ xample-projects/coverage-example/liftwizard-reladomo-test-resource-writer-tests/src/test/java/io/liftwizard/reladomo/test/resource/writer/tests/ReladomoTestResourceWriterTest.java
@@ -57,17 +57,21 @@ public class ReladomoTestResourceWriterTest
         String actual = ReladomoTestResourceWriter.generate();
         String expected = ""
                 + "class cool.klass.xample.coverage.PropertiesOptional\n"
-                + "propertiesOptionalId, optionalString                         , optionalInteger, optionalLong, optionalDouble, optionalFloat, optionalBoolean, optionalInstant        , optionalLocalDate, createdById  , createdOn                , lastUpdatedById, systemFrom               , systemTo               \n"
-                + "                   1, \"PropertiesOptional optionalString 1 ☝\",               1, 100000000000,   1.0123456789,     1.0123457,            true, \"1999-12-31 23:59:00.0\", \"1999-12-31\"     , \"test user 1\", \"1999-12-31 23:59:59.999\", \"test user 1\"  , \"1999-12-31 23:59:59.999\", \"9999-12-01 23:59:00.0\"\n"
+                + "systemFrom               , systemTo               , propertiesOptionalId, optionalString      , optionalInteger, optionalLong, optionalDouble, optionalFloat, optionalBoolean, optionalInstant        , optionalLocalDate, createdById  , createdOn                , lastUpdatedById\n"
+                + "\"1999-12-31 23:59:59.999\", \"9999-12-01 23:59:00.0\",                    1, \"optionalString 1 ☝\",               1, 100000000000,   1.0123456789,     1.0123457,            true, \"1999-12-31 23:59:00.0\", \"1999-12-31\"     , \"test user 1\", \"1999-12-31 23:59:59.999\", \"test user 1\"  \n"
+                + "\"1999-12-31 23:59:59.999\", \"9999-12-01 23:59:00.0\",                    2, null                ,            null,         null,           null,          null,            null, null                   , null             , \"test user 1\", \"1999-12-31 23:59:59.999\", \"test user 1\"  \n"
+                + "\n"
                 + "class cool.klass.xample.coverage.PropertiesOptionalVersion\n"
-                + "propertiesOptionalId, number, createdById  , createdOn                , lastUpdatedById, systemFrom               , systemTo               \n"
-                + "                   1,      1, \"test user 1\", \"1999-12-31 23:59:59.999\", \"test user 1\"  , \"1999-12-31 23:59:59.999\", \"9999-12-01 23:59:00.0\"\n"
+                + "systemFrom               , systemTo               , propertiesOptionalId, number, createdById  , createdOn                , lastUpdatedById\n"
+                + "\"1999-12-31 23:59:59.999\", \"9999-12-01 23:59:00.0\",                    1,      1, \"test user 1\", \"1999-12-31 23:59:59.999\", \"test user 1\"  \n"
+                + "\n"
                 + "class cool.klass.xample.coverage.PropertiesRequired\n"
-                + "propertiesRequiredId, requiredString                         , requiredInteger, requiredLong, requiredDouble, requiredFloat, requiredBoolean, requiredInstant        , requiredLocalDate, createdById  , createdOn                , lastUpdatedById, systemFrom               , systemTo               \n"
-                + "                   1, \"PropertiesRequired requiredString 1 ☝\",               1, 100000000000,   1.0123456789,     1.0123457,            true, \"1999-12-31 23:59:00.0\", \"1999-12-31\"     , \"test user 1\", \"1999-12-31 23:59:59.999\", \"test user 1\"  , \"1999-12-31 23:59:59.999\", \"9999-12-01 23:59:00.0\"\n"
+                + "systemFrom               , systemTo               , propertiesRequiredId, requiredString      , requiredInteger, requiredLong, requiredDouble, requiredFloat, requiredBoolean, requiredInstant        , requiredLocalDate, createdById  , createdOn                , lastUpdatedById\n"
+                + "\"1999-12-31 23:59:59.999\", \"9999-12-01 23:59:00.0\",                    1, \"requiredString 1 ☝\",               1, 100000000000,   1.0123456789,     1.0123457,            true, \"1999-12-31 23:59:00.0\", \"1999-12-31\"     , \"test user 1\", \"1999-12-31 23:59:59.999\", \"test user 1\"  \n"
+                + "\n"
                 + "class cool.klass.xample.coverage.PropertiesRequiredVersion\n"
-                + "propertiesRequiredId, number, createdById  , createdOn                , lastUpdatedById, systemFrom               , systemTo               \n"
-                + "                   1,      1, \"test user 1\", \"1999-12-31 23:59:59.999\", \"test user 1\"  , \"1999-12-31 23:59:59.999\", \"9999-12-01 23:59:00.0\"\n";
+                + "systemFrom               , systemTo               , propertiesRequiredId, number, createdById  , createdOn                , lastUpdatedById\n"
+                + "\"1999-12-31 23:59:59.999\", \"9999-12-01 23:59:00.0\",                    1,      1, \"test user 1\", \"1999-12-31 23:59:59.999\", \"test user 1\"  \n";
         assertEquals(expected, actual);
     }
 }
