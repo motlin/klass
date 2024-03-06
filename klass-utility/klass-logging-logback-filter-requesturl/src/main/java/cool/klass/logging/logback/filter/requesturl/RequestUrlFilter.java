dd4bebf5b0170ccf03b7a7a5fa08fdd0bc2b0197
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Add module klass-dropwizard-configuration-logging-filter-url.

--- klass-utility/klass-logging-logback-filter-requesturl/src/main/java/cool/klass/logging/logback/filter/requesturl/RequestUrlFilter.java
+++ klass-utility/klass-logging-logback-filter-requesturl/src/main/java/cool/klass/logging/logback/filter/requesturl/RequestUrlFilter.java
@@ -5,28 +5,26 @@ import java.util.Objects;
 import ch.qos.logback.access.spi.IAccessEvent;
 import ch.qos.logback.core.filter.Filter;
 import ch.qos.logback.core.spi.FilterReply;
+import org.eclipse.collections.api.factory.Lists;
 import org.eclipse.collections.api.list.ImmutableList;
 
 public class RequestUrlFilter extends Filter<IAccessEvent>
 {
     private final ImmutableList<String> bannedUrls;
 
-    public RequestUrlFilter(ImmutableList<String> bannedUrls)
+    public RequestUrlFilter(Iterable<String> bannedUrls)
     {
-        this.bannedUrls = Objects.requireNonNull(bannedUrls);
+        Objects.requireNonNull(bannedUrls);
+        this.bannedUrls = Lists.immutable.withAll(bannedUrls);
     }
 
     @Override
     public FilterReply decide(IAccessEvent event)
     {
-        String url = event.getRequestURL();
-        for (String bannedUrl : this.bannedUrls)
-        {
-            if (url.contains(bannedUrl))
-            {
-                return FilterReply.DENY;
-            }
-        }
-        return FilterReply.NEUTRAL;
+        String requestURL = event.getRequestURL();
+
+        return this.bannedUrls.anySatisfy(requestURL::contains)
+                ? FilterReply.DENY
+                : FilterReply.NEUTRAL;
     }
 }
