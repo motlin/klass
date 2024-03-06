a55a5e309f98e53b422fa7add0a220c6970e8dca
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Sync archetype.

--- klass-maven-archetype/src/main/resources/archetype-resources/__rootArtifactId__-dropwizard-application/src/main/java/dropwizard/application/__name__Application.java
+++ klass-maven-archetype/src/main/resources/archetype-resources/__rootArtifactId__-dropwizard-application/src/main/java/dropwizard/application/__name__Application.java
@@ -2,10 +2,14 @@ package ${package}.dropwizard.application;
 
 import javax.annotation.Nonnull;
 
+import cool.klass.serialization.jackson.module.meta.model.module.KlassMetaModelJacksonModule;
+import cool.klass.servlet.filter.mdc.jsonview.JsonViewDynamicFeature;
+import cool.klass.servlet.logging.structured.klass.response.KlassResponseStructuredLoggingFilter;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
-public class ${name}Application extends Abstract${name}Application
+public class ${name}Application
+        extends Abstract${name}Application
 {
     public static void main(String[] args) throws Exception
     {
@@ -21,8 +25,25 @@ public class ${name}Application extends Abstract${name}Application
     }
 
     @Override
+    protected void registerLoggingFilters(@Nonnull Environment environment)
+    {
+        super.registerLoggingFilters(environment);
+
+        environment.jersey().register(KlassResponseStructuredLoggingFilter.class);
+        environment.jersey().register(JsonViewDynamicFeature.class);
+    }
+
+    @Override
+    protected void registerJacksonModules(@Nonnull Environment environment)
+    {
+        super.registerJacksonModules(environment);
+
+        environment.getObjectMapper().registerModule(new KlassMetaModelJacksonModule());
+    }
+
+    @Override
     public void run(
-            ${name}Configuration configuration,
+            @Nonnull ${name}Configuration configuration,
             @Nonnull Environment environment) throws Exception
     {
         super.run(configuration, environment);
--- klass-maven-archetype/src/main/resources/archetype-resources/__rootArtifactId__-dropwizard-application/src/main/java/dropwizard/application/__name__Configuration.java
+++ klass-maven-archetype/src/main/resources/archetype-resources/__rootArtifactId__-dropwizard-application/src/main/java/dropwizard/application/__name__Configuration.java
@@ -2,7 +2,8 @@ package ${package}.dropwizard.application;
 
 import cool.klass.dropwizard.configuration.AbstractKlassConfiguration;
 
-public class ${name}Configuration extends AbstractKlassConfiguration
+public class ${name}Configuration
+        extends AbstractKlassConfiguration
 {
     // TODO: implement service configuration
 }
