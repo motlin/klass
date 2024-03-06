baf96e209a109cd7221e21d9b2a9db3a8060fb32
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Swap hocon for dropwizard configuration.

--- klass-dropwizard/klass-dropwizard-configuration-factories/klass-dropwizard-configuration-sample-data/src/main/java/cool/klass/dropwizard/configuration/sample/data/SampleDataFactoryProvider.java
+++ klass-dropwizard/klass-dropwizard-configuration-factories/klass-dropwizard-configuration-sample-data/src/main/java/cool/klass/dropwizard/configuration/sample/data/SampleDataFactoryProvider.java
@@ -1,13 +1,6 @@
 package cool.klass.dropwizard.configuration.sample.data;
 
-import cool.klass.dropwizard.configuration.data.store.DataStoreFactory;
-import cool.klass.dropwizard.configuration.domain.model.loader.DomainModelFactory;
-
 public interface SampleDataFactoryProvider
 {
     SampleDataFactory getSampleDataFactory();
-
-    DataStoreFactory getDataStoreFactory();
-
-    DomainModelFactory getDomainModelFactory();
 }
