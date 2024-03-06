546ab7ec6ab4876187ca14a07c52e692f70b64bf
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Add module klass-servlet-logging-structured-status-info.

---
+++ klass-servlet/klass-servlet-logging-structured/klass-servlet-logging-structured-status-info/src/main/java/cool/klass/servlet/logging/structured/status/info/StatusInfoStructuredLoggingFilter.java
@@ -0,0 +1,38 @@
+package cool.klass.servlet.logging.structured.status.info;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.Map;
+import java.util.Objects;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.Response.StatusType;
+
+public class StatusInfoStructuredLoggingFilter implements ContainerResponseFilter
+{
+    @Override
+    public void filter(
+            ContainerRequestContext requestContext,
+            ContainerResponseContext responseContext) throws IOException
+    {
+        Object structuredArguments = requestContext.getProperty("structuredArguments");
+        Objects.requireNonNull(structuredArguments);
+        Map<String, Object> structuredArgumentsMap = (Map<String, Object>) structuredArguments;
+
+        StatusType statusInfo = responseContext.getStatusInfo();
+        if (statusInfo.getStatusCode() != responseContext.getStatus())
+        {
+            throw new AssertionError();
+        }
+
+        structuredArgumentsMap.put("klass.response.http.statusEnum", statusInfo.toEnum());
+        structuredArgumentsMap.put("klass.response.http.statusCode", statusInfo.getStatusCode());
+        structuredArgumentsMap.put("klass.response.http.statusFamily", statusInfo.getFamily());
+        structuredArgumentsMap.put("klass.response.http.statusPhrase", statusInfo.getReasonPhrase());
+
+        Type entityType = responseContext.getEntityType();
+        structuredArgumentsMap.put("klass.response.http.entityType", entityType);
+    }
+}
