05118234e63675b1f8dd18c59758460ac5c703f6
Craig P. Motlin <cmotlin@gmail.com>
2024-03-06 00:00:00 +0000

    Add module klass-generator-projection-plugin.

---
+++ klass-generator-plugins/klass-generator-service-plugin/src/main/java/cool/klass/generator/klass/service/plugin/GenerateServiceMojo.java
@@ -0,0 +1,34 @@
+package cool.klass.generator.klass.service.plugin;
+
+import java.io.File;
+
+import cool.klass.generator.klass.service.KlassServiceGenerator;
+import cool.klass.generator.plugin.AbstractGenerateMojo;
+import cool.klass.model.meta.domain.api.DomainModel;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+
+@Mojo(
+        name = "generate-klass-service",
+        defaultPhase = LifecyclePhase.GENERATE_SOURCES,
+        threadSafe = true,
+        requiresDependencyResolution = ResolutionScope.RUNTIME)
+public class GenerateServiceMojo
+        extends AbstractGenerateMojo
+{
+    @Parameter(
+            property = "outputDirectory",
+            defaultValue = "${project.build.directory}/generated-sources/service")
+    private File outputDirectory;
+
+    @Override
+    public void execute() throws MojoExecutionException
+    {
+        DomainModel domainModel = this.getDomainModel();
+        KlassServiceGenerator generator = new KlassServiceGenerator(domainModel);
+        generator.writeKlassFiles(this.outputDirectory.toPath());
+    }
+}
