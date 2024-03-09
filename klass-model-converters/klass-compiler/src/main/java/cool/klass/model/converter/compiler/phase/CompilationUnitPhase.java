2a1f3468ed904e63f991c691153644e85de7dac7
Craig P. Motlin <cmotlin@gmail.com>
2024-03-09 10:29:35 -0500

    Add an assertion to CompilationUnitPhase.enterCompilationUnit().

--- klass-model-converters/klass-compiler/src/main/java/cool/klass/model/converter/compiler/phase/CompilationUnitPhase.java
+++ klass-model-converters/klass-compiler/src/main/java/cool/klass/model/converter/compiler/phase/CompilationUnitPhase.java
@@ -4,11 +4,13 @@ import java.util.Optional;
 
 import javax.annotation.Nonnull;
 
+import cool.klass.model.converter.compiler.CompilationUnit;
 import cool.klass.model.converter.compiler.CompilerState;
 import cool.klass.model.converter.compiler.state.AntlrCompilationUnit;
 import cool.klass.model.converter.compiler.state.AntlrPackage;
 import cool.klass.model.meta.grammar.KlassParser.CompilationUnitContext;
 import cool.klass.model.meta.grammar.KlassParser.PackageDeclarationContext;
+import org.antlr.v4.runtime.ParserRuleContext;
 
 public class CompilationUnitPhase
         extends AbstractCompilerPhase
@@ -25,9 +27,16 @@ public class CompilationUnitPhase
     {
         super.enterCompilationUnit(ctx);
 
+        CompilationUnit currentCompilationUnit = this.compilerState.getCompilerWalk().getCurrentCompilationUnit();
+        ParserRuleContext parserContext        = currentCompilationUnit.getParserContext();
+        if (ctx != parserContext)
+        {
+            throw new AssertionError();
+        }
+
         this.compilationUnitState = new AntlrCompilationUnit(
                 ctx,
-                Optional.of(this.compilerState.getCompilerWalk().getCurrentCompilationUnit()));
+                Optional.of(currentCompilationUnit));
     }
 
     @Override
